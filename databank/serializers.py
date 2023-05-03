8a9b82a52c7cab8f8d1d8f0f37b34f89439f3c02
Gualter Perinho <programadoracoriano@gmail.com>
2023-05-03 11:20:39 +0000

    fix profile

--- databank/serializers.py
+++ databank/serializers.py
@@ -35,7 +35,6 @@ class UserSerializer(serializers.ModelSerializer):
         return str(refresh.access_token)
 
 class ProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
     class Meta:
         model = Profile
         fields = '__all__'
