b9d7a001ccb45744d80d3185849db2c9088670ae
Gualter Perinho <programadoracoriano@gmail.com>
2023-05-03 17:18:02 +0000

    changing datatype

--- databank/views.py
+++ databank/views.py
@@ -39,15 +39,15 @@ class AddFundsView(generics.UpdateAPIView):
         return Profile.objects.filter(user=self.request.user)
 
     def post(self, request, *args, **kwargs):
-        cc_number       = '4242 4242 4242 4242'
-        cc_date_month   = '12'
-        cc_date_year    = '2024'
-        cc_cvc          = '123'
+        cc_number       = 4242424242424242
+        cc_date_month   = 12
+        cc_date_year    = 2024
+        cc_cvc          = 123
         card_number     = request.data.get('card_number')
         card_date_month = request.data.get('card_date_month')
         card_date_year  = request.data.get('card_date_year')
         card_cvc        = request.data.get('card_cvc')
-        funds           = request.data.get('funds')
+        funds           = float(request.data.get('funds'))
         condition = ((card_number == cc_number and card_date_month == cc_date_month
                      and card_date_year == cc_date_year and card_cvc == cc_cvc) and
                      funds > 0 and funds is not None)
