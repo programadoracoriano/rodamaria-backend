0af61b0fe3db653ea95c5fb35d379f9f9f0d2dba
Gualter Perinho <programadoracoriano@gmail.com>
2023-05-03 16:39:18 +0000

    editing admin

--- databank/admin.py
+++ databank/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    model           = Profile
+    list_display    = ('id')
+    search_fields   = ('id','name', )
+
 
 admin.site.register(Profile)
